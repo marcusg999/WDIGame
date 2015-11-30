f6703c489491a52d57d0d4c742fc8643e891157e
marcus gray <abundanceisfree@gmail.com>
2015-11-30 12:14:38 -0800

    3rd commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
               var c=document.getElementById("myCanvas");
               var ctx=c.getContext("2d");
               var img=document.getElementById("Vinyl_Tbg");
-              ctx.drawImage(img,60,100);
-
-               var c=document.getElementById("myCanvas");
-              var ctx=c.getContext("2d");
-              var img=document.getElementById("paddle");
               ctx.drawImage(img,10,10);
+
+              var canvas = document.getElementById('myCanvas');
+              var context = canvas.getContext('2d');
+              var x = 108;
+              var y = 370;
+              var width = 220;
+              var height = 28;
+              var imageObj = new Image();
+              imageObj.onload = function() {
+                context.drawImage(imageObj, x, y, width, height);
+              };
+              imageObj.src = "images/turntable.jpg";
+
