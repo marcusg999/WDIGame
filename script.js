6f6066853dfe4ce48c7b70a9cd9b46a80c11e757
marcus gray <abundanceisfree@gmail.com>
2015-11-30 15:46:19 -0800

    5th commit

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
+              var canvas = document.getElementById('myCanvas');
+              var context = canvas.getContext('2d');
+              var x = 188;
+              var y = 39;
+              var width = 250;
+              var height = 37;
+              var imageObj = new Image();
+              imageObj.onload = function() {
+              context.drawImage(imageObj, x, y, width, height);
+              };
+              imageObj.src = "images/turntable.jpg";
+
               var c=document.getElementById("myCanvas");
               var ctx=c.getContext("2d");
               var img=document.getElementById("Vinyl_Tbg");
               ctx.drawImage(img,10,10);
 
               var canvas = document.getElementById('myCanvas');
-              var context = canvas.getContext('2d');
-              var x = 108;
-              var y = 369;
-              var width = 220;
-              var height = 28;
-              var imageObj = new Image();
-              imageObj.onload = function() {
-              context.drawImage(imageObj, x, y, width, height);
-              };
-
-              imageObj.src = "images/turntable.jpg";
-
-              var canvas = document.getElementById("myCanvas");
               var ctx = canvas.getContext("2d");
               var x = canvas.width/2;
               var y = canvas.height-30;
               var dx = 2;
               var dy = -2;
 
-              function draw (){
+              var canvas = document.getElementById("myCanvas");
+              var ctx = canvas.getContext("2d");
+
+
               ctx.beginPath();
-              ctx.arc(x, y, 10, 0, Math.PI*2);
+              ctx.arc(240, 160, 8, 0, Math.PI*2, false);
               ctx.fillStyle = "white";
               ctx.fill();
               ctx.closePath();
-              x += dx;
-              y += dy;
-            }
-              setInterval(draw,10);
+
+              ctx.beginPath();
+              ctx.paddle(160, 10, 100, 40);
+              ctx.stroke();
+              ctx.closePath();
 
 
 
