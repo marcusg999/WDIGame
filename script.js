c64d1e641a3cdff3519ffea6e166b46b0f53acb6
marcus gray <abundanceisfree@gmail.com>
2015-11-30 11:11:21 -0800

    2nd commit

---
+++ script.js
@@ -0,0 +1,9 @@
+              var c=document.getElementById("myCanvas");
+              var ctx=c.getContext("2d");
+              var img=document.getElementById("Vinyl_Tbg");
+              ctx.drawImage(img,60,100);
+
+               var c=document.getElementById("myCanvas");
+              var ctx=c.getContext("2d");
+              var img=document.getElementById("paddle");
+              ctx.drawImage(img,10,10);
